b4b9d366b71cc2a25a2ce3d179bfdcc749ecefab
Cesar Zamora <44296157+czammar@users.noreply.github.com>
2025-07-01 13:49:09 -0600

    feat: pydantic models for bot

---
+++ src/bot/models/request.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ChatbotRequestModel(BaseModel):
+    """
+    Request model for dictamination processing.
+
+    Attributes:
+        text (str):
+            The input text to be processed.
+        thread_id (Optional[str]):
+            A unique identifier for the conversation thread. If None, a
+            new thread may be created automatically.
+    """
+    text: str
+    checkpointer_id: Optional[str] = None
